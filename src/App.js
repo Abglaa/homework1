7fef3348ffe033352b253e6c78a23af5357efdc1
Abglaa <Altymysbaevabdulaziz@gmail.com>
2023-11-09 16:27:20 +0600

    homework1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function MyAppy() {
+  const [count, setCount] = useState(0)
+
+  function handleClick() {
+    setCount(count + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>dfbhfhebffjdfjfsadf</h1>
+        <MyButton count={count} onClick={handleClick} />
+        <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
-
-export default App;
+function MyButton({ count, onClick}) {
+  return <button onClick={onClick}>Cliclked {count} times!</button>;
+}
